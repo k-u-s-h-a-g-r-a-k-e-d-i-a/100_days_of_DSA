a2bf2035c2a6f68edcf0133c884f4502531a8624
~Kush <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2026-02-16 16:59:52 +0530

    Create Day5.c

---
+++ Day5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+    int arr1[n1], arr2[n2], merged[n1+n2];
+
+    for(int i=0; i<n1; i++) scanf("%d", &arr1[i]);
+    for(int i=0; i<n2; i++) scanf("%d", &arr2[i]);
+
+    int i=0, j=0, k=0;
+    while(i<n1 && j<n2) {
+        if(arr1[i] <= arr2[j]) merged[k++] = arr1[i++];
+        else merged[k++] = arr2[j++];
+    }
+    while(i<n1) merged[k++] = arr1[i++];
+    while(j<n2) merged[k++] = arr2[j++];
+
+    for(int x=0; x<n1+n2; x++) printf("%d ", merged[x]);
+    return 0;
+}
