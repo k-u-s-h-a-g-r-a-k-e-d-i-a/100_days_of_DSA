07b2cfa98360fc4137c3214845dd021fceb4df8f
~Kush <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2026-02-22 14:41:30 +0530

    Create Day18.c

---
+++ Day18.c
@@ -0,0 +1,19 @@
+//Find Maximum and Minimum Element
+#include <stdio.h>
+int main(){
+    int n;
+    int arr[n];
+    int min = 0;
+    int max=0;
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]<min)
+            min = arr[i];
+        else if(arr[i]>max)
+            max = arr[i];
+    }
+    printf("max:%d,Min:%d",max,min);
+    return 0;
+}
