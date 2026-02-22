6c666d00b45fca2fb1f648e3ac16be9912b544f8
~Kush <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2026-02-22 14:19:08 +0530

    Update Day17.c

--- Day17.c
+++ Day17.c
@@ -3,11 +3,12 @@ int main(){
     int n;
     scanf("%d",&n);
     int arr[n];
+    int freq[100]; //let frequency of each element be less than 100.
     for(int i=0;i<n;i++){
-        scanf("%d",arr[i]);
+        scanf("%d",&arr[i]);
         freq[arr[i]]++;
     }
-    int freq[100]; //let frequency of each element be less than 100.
+    
     for(int i =0;i<n;i++){
         if(arr[i]>0){
             printf("%d,%d",arr[i],freq[arr[i]]);
