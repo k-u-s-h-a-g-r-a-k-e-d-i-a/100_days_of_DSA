a7732224511778ad22ef9488653e3c7cdb4e97e6
~Kush <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2026-03-15 12:03:07 +0530

    Update Day22.c

--- Day22.c
+++ Day22.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct Node {
     int data;
     struct Node* next;
 };
 
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
-}
-
-int countOccurrences(struct Node* head, int key) {
+int countNodes(struct Node* head) {
     int count = 0;
-    struct Node* current = head;
-    
-    while (current != NULL) {
-        if (current->data == key) {
-            count++;
-        }
-        current = current->next;
+    while (head != NULL) {
+        count++;
+        head = head->next;
     }
-    
     return count;
 }
 
 int main() {
-    int n, value, key;
+    int n, value;
+    scanf("%d", &n);
+    
     struct Node* head = NULL;
     struct Node* tail = NULL;
     
-    scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &value);
-        struct Node* newNode = createNode(value);
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+        
         if (head == NULL) {
             head = tail = newNode;
         } else {
@@ -44,7 +35,6 @@ int main() {
         }
     }
     
-    scanf("%d", &key);
-    printf("%d\n", countOccurrences(head, key));
+    printf("%d\n", countNodes(head));
     return 0;
 }
